e1f965414da9231b44805e53e9824486b32896cb
unknown <islam.sa3idzak@gmail.com>
2021-05-31 23:53:54 +0200

    this is last test

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
+
 // Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/game-space'));
- 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/game-space/index.html'));
-});
+app.use(express.static('./dist/game-space'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/game-space/'}),
+);
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
